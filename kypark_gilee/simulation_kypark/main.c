ce1089a29a2ee38802c932857141da3061339aa5
gilee <gilee@student.42seoul.kr>
2021-10-21 18:03:10 +0900

    update simulation

---
+++ kypark_gilee/simulation_kypark/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include "../queue/queue.h"
+
+int main(void)
+{
+  Queue *watingQ = createQueue(10);
+  Queue *arrivedQ = createQueue(10);
+
+  
+  return 0;
+}
